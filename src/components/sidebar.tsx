4518af1d16c7bf770bb3e4ca206df1106cb20573
Nguyễn Ngọc Cương <cuongnguyen71195@gmail.com>
2025-11-08 22:09:22 +0700

    nmbn,

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -6,6 +6,7 @@ import ProfileModal from "./profilemodal";
 import PricingModal from "./pricingModal";
 import { useUser } from "../contexts/userscontext";
 import { motion, AnimatePresence } from "framer-motion";
+import HowtoUse from "./howtouse";
 
 function convertStyleStringToObject(styleString: string) {
   const styleObject: { [key: string]: string } = {};
@@ -31,6 +32,7 @@ interface SidebarProps {
 export default function SideBar({ isOpen, onClose }: SidebarProps) {
   const router = useRouter();
   const [openPricingModal, setOpenPricingModal] = useState(false);
+  const [openHowtoUse, setOpenHowtoUse] = useState(false);
   const [openModalCreate, setOpenModalCreate] = useState(false);
   const { user } = useUser();
   const percentage = Math.min(
@@ -68,7 +70,7 @@ export default function SideBar({ isOpen, onClose }: SidebarProps) {
                 <button
                   disabled={(user?.post_used ?? 0) >= (user?.post_limit ?? 0)}
                   onClick={() => setOpenModalCreate(true)}
-                  className="inline-flex items-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 hover:bg-accent h-10 px-4 w-full justify-center bg-gradient-to-r from-[#B8D8FF] to-[#85ACFF] hover:opacity-80 rounded-xl max-h-[600px]:rounded text-white hover:text-white text-[10px] max-h-[600px]:text-[8px] sm:text-[11px] md:text-[12px] xl:text-sm py-0.5 max-h-[600px]:py-px sm:py-1 xl:py-1.5"
+                  className="btn-primary inline-flex items-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 hover:bg-accent h-10 px-4 w-full justify-center bg-gradient-to-r from-[#B8D8FF] to-[#85ACFF] hover:opacity-80 rounded-xl max-h-[600px]:rounded text-white hover:text-white text-[10px] max-h-[600px]:text-[8px] sm:text-[11px] md:text-[12px] xl:text-sm py-0.5 max-h-[600px]:py-px sm:py-1 xl:py-1.5"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -251,6 +253,33 @@ export default function SideBar({ isOpen, onClose }: SidebarProps) {
                   isOpen={openPricingModal}
                   onClose={() => setOpenPricingModal(false)}
                 ></PricingModal>
+                <button
+                  onClick={() => handleClick("/dashboard/howtouse")}
+                  className="inline-flex items-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 h-10 px-4 w-full rounded-xl max-h-[600px]:rounded justify-start text-gray-600 hover:text-gray-900 hover:bg-[#EEEFE8] text-[10px] max-h-[600px]:text-[8px] sm:text-[11px] md:text-[12px] xl:text-sm py-0.5 max-h-[600px]:py-px sm:py-1 xl:py-1.5"
+                  data-feedback-fish="true"
+                  data-feedback-fish-userid="cuongnguyen71195@gmail.com"
+                >
+                  <svg
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="mr-1 max-h-[600px]:mr-0.5 sm:mr-1.5 h-2 w-2 max-h-[600px]:h-1.5 max-h-[600px]:w-1.5 sm:h-2.5 sm:w-2.5 xl:h-3 xl:w-3"
+                  >
+                    <path
+                      d="M0.877075 7.49972C0.877075 3.84204 3.84222 0.876892 7.49991 0.876892C11.1576 0.876892 14.1227 3.84204 14.1227 7.49972C14.1227 11.1574 11.1576 14.1226 7.49991 14.1226C3.84222 14.1226 0.877075 11.1574 0.877075 7.49972ZM7.49991 1.82689C4.36689 1.82689 1.82708 4.36671 1.82708 7.49972C1.82708 10.6327 4.36689 13.1726 7.49991 13.1726C10.6329 13.1726 13.1727 10.6327 13.1727 7.49972C13.1727 4.36671 10.6329 1.82689 7.49991 1.82689ZM8.24993 10.5C8.24993 10.9142 7.91414 11.25 7.49993 11.25C7.08571 11.25 6.74993 10.9142 6.74993 10.5C6.74993 10.0858 7.08571 9.75 7.49993 9.75C7.91414 9.75 8.24993 10.0858 8.24993 10.5ZM6.05003 6.25C6.05003 5.57211 6.63511 4.925 7.50003 4.925C8.36496 4.925 8.95003 5.57211 8.95003 6.25C8.95003 6.74118 8.68002 6.99212 8.21447 7.27494C8.16251 7.30651 8.10258 7.34131 8.03847 7.37854L8.03841 7.37858C7.85521 7.48497 7.63788 7.61119 7.47449 7.73849C7.23214 7.92732 6.95003 8.23198 6.95003 8.7C6.95004 9.00376 7.19628 9.25 7.50004 9.25C7.8024 9.25 8.04778 9.00601 8.05002 8.70417L8.05056 8.7033C8.05924 8.6896 8.08493 8.65735 8.15058 8.6062C8.25207 8.52712 8.36508 8.46163 8.51567 8.37436L8.51571 8.37433C8.59422 8.32883 8.68296 8.27741 8.78559 8.21506C9.32004 7.89038 10.05 7.35382 10.05 6.25C10.05 4.92789 8.93511 3.825 7.50003 3.825C6.06496 3.825 4.95003 4.92789 4.95003 6.25C4.95003 6.55376 5.19628 6.8 5.50003 6.8C5.80379 6.8 6.05003 6.55376 6.05003 6.25Z"
+                      fill="currentColor"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  How to use
+                </button>
+                <HowtoUse
+                  isOpen={openHowtoUse}
+                  onClose={() => setOpenHowtoUse(false)}
+                ></HowtoUse>
                 <Link
                   href="https://t.me/cuongnguyencnn1"
                   target="_blank"
@@ -564,6 +593,28 @@ export default function SideBar({ isOpen, onClose }: SidebarProps) {
                   isOpen={openPricingModal}
                   onClose={() => setOpenPricingModal(false)}
                 ></PricingModal>
+                <button
+                  className="inline-flex items-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 h-10 px-4 w-full rounded-xl max-h-[600px]:rounded justify-start text-gray-600 hover:text-gray-900 hover:bg-[#EEEFE8] text-[10px] max-h-[600px]:text-[8px] sm:text-[11px] md:text-[12px] xl:text-sm py-0.5 max-h-[600px]:py-px sm:py-1 xl:py-1.5"
+                  data-feedback-fish="true"
+                  data-feedback-fish-userid="cuongnguyen71195@gmail.com"
+                >
+                  <svg
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="mr-1 max-h-[600px]:mr-0.5 sm:mr-1.5 h-2 w-2 max-h-[600px]:h-1.5 max-h-[600px]:w-1.5 sm:h-2.5 sm:w-2.5 xl:h-3 xl:w-3"
+                  >
+                    <path
+                      d="M0.877075 7.49972C0.877075 3.84204 3.84222 0.876892 7.49991 0.876892C11.1576 0.876892 14.1227 3.84204 14.1227 7.49972C14.1227 11.1574 11.1576 14.1226 7.49991 14.1226C3.84222 14.1226 0.877075 11.1574 0.877075 7.49972ZM7.49991 1.82689C4.36689 1.82689 1.82708 4.36671 1.82708 7.49972C1.82708 10.6327 4.36689 13.1726 7.49991 13.1726C10.6329 13.1726 13.1727 10.6327 13.1727 7.49972C13.1727 4.36671 10.6329 1.82689 7.49991 1.82689ZM8.24993 10.5C8.24993 10.9142 7.91414 11.25 7.49993 11.25C7.08571 11.25 6.74993 10.9142 6.74993 10.5C6.74993 10.0858 7.08571 9.75 7.49993 9.75C7.91414 9.75 8.24993 10.0858 8.24993 10.5ZM6.05003 6.25C6.05003 5.57211 6.63511 4.925 7.50003 4.925C8.36496 4.925 8.95003 5.57211 8.95003 6.25C8.95003 6.74118 8.68002 6.99212 8.21447 7.27494C8.16251 7.30651 8.10258 7.34131 8.03847 7.37854L8.03841 7.37858C7.85521 7.48497 7.63788 7.61119 7.47449 7.73849C7.23214 7.92732 6.95003 8.23198 6.95003 8.7C6.95004 9.00376 7.19628 9.25 7.50004 9.25C7.8024 9.25 8.04778 9.00601 8.05002 8.70417L8.05056 8.7033C8.05924 8.6896 8.08493 8.65735 8.15058 8.6062C8.25207 8.52712 8.36508 8.46163 8.51567 8.37436L8.51571 8.37433C8.59422 8.32883 8.68296 8.27741 8.78559 8.21506C9.32004 7.89038 10.05 7.35382 10.05 6.25C10.05 4.92789 8.93511 3.825 7.50003 3.825C6.06496 3.825 4.95003 4.92789 4.95003 6.25C4.95003 6.55376 5.19628 6.8 5.50003 6.8C5.80379 6.8 6.05003 6.55376 6.05003 6.25Z"
+                      fill="currentColor"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  How to use
+                </button>
                 <button
                   className="inline-flex items-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 h-10 px-4 w-full rounded-xl max-h-[600px]:rounded justify-start text-gray-600 hover:text-gray-900 hover:bg-[#EEEFE8] text-[10px] max-h-[600px]:text-[8px] sm:text-[11px] md:text-[12px] xl:text-sm py-0.5 max-h-[600px]:py-px sm:py-1 xl:py-1.5"
                   data-feedback-fish="true"
